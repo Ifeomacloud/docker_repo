a95066d200e11abea72a296b2cc63bdaaa251758
Ifeoma Okoye <ifeomaokoyequeen@gmail.com>
2025-03-23 16:12:29 +0000

    Update urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+# Temporary view for testing the home page
+def home_view(request):
+    return HttpResponse("Welcome to the Home Page!")
 
 urlpatterns = [
+    path('', home_view, name='home'),  # This fixes the 404 error
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
 ]
